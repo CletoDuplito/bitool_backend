fdd2388e00810331859bd447d6d6a7a4c9834a3f
DuyNguyen <blinkblinklink@aim.com>
2016-05-11 15:03:33 -0700

    progress

--- cs157B_Project/frontend/js/controllers.js
+++ cs157B_Project/frontend/js/controllers.js
@@ -4,14 +4,18 @@ var appControllers = angular.module("appControllers",[]);
 
 appControllers.
 	controller("CentreCubeCtrl",["$scope","$http","$routeParams","$timeout","FormData", function($scope,$http,$routeParams,$timeout,formData) {
-		var url = "api/bi/parser";
-		// var url = "data/data.json";
-		var parsedParam = formData.parseData( formData.defaultFormValues );
+		// var url = "api/bi/parser";
+		var url = "data/data.json";
+		var parsedParam = formData.parseParam( formData.defaultFormValues );
 
 		parsedParam["action"] = "central_cube"; //default action
 
 		$http.get(url, {params: parsedParam}).success(function(data){
 			$scope.data = data;
+			console.log(data);
+			// $scope.cardinals = formData.parseCardinalValues(data);
+			$scope.$emit("new_cardinals", formData.parseCardinalValues(data));
+			// console.log($scope.cardinals);
 		});
 		$scope.visible = true;
 		$scope.$on("data_changed", getNewData);		//watch data changes
@@ -34,17 +38,29 @@ appControllers.
 	controller("OptionsCtrl",["$scope","$http","$routeParams","FormData", function($scope,$http,$routeParams,formData) {
 		$scope.form = formData.defaultFormValues;
 		$scope.disable = false;
-		
+		$scope.dimensionError = false;
+		$scope.cardinalError = false;
+
+		$scope.$on("new_cardinals", function(e, cardinals) {		//listen for cardinal values
+			$scope.form.cardinals = cardinals;
+		})
 
 		$scope.changeView = function(d) {
+			var parsedFormParams = formData.parseParam(d);
+			if(!parsedFormParams) {
+				$scope.cardinalError = true;
+				return;
+			}
+
 			//broadcast it to centrecubctrl
-			$scope.$broadcast("data_changed", formData.parseData(d) );
+			$scope.$broadcast("data_changed", parsedFormParams );
 		};
 
 		$scope.dimensionCheck = function(dim) {
 			var keys = Object.keys(dim);
 			var count = 0;
 			$scope.disable = false;
+			$scope.dimensionError = false;
 
 			for(var i = 0; i < keys.length; i++) {
 				var currentValue = dim[ keys[i] ];
@@ -54,7 +70,7 @@ appControllers.
 			}
 			if(count >= 2) {
 				$scope.disable = true;
+				$scope.dimensionError = true;
 			}
 		}
-
 	}]);
--- cs157B_Project/frontend/js/main.js
+++ cs157B_Project/frontend/js/main.js
@@ -8,37 +8,93 @@ app.factory("FormData", function(){
 	var defaultFormValues = {		//init form values
 		action: "roll_up",
 		dimension: {
-			datetime: true,
+			date_time: true,
 			store: true,
 			product: true
 		},
 		category: {
-			datetime: "year",
-			store: "store_zip",
-			product: "department"
-		}
+			date_time: "year",
+			store: "store_state",
+			product: "category"
+		},
+		cardinals: {}
 	};
 
-	var parseData = function(d) {
+	/*
+	 * Parse the dimension params
+	 */
+	var parseParam = function(d) {
 		//create query parameters
-		var attributes = [];
-		Object.keys(d.dimension).forEach(function(value){
-
-			if(d.dimension[value]) { 				//check if dimension is selected				
-				attributes.push( [ value,d.category[value] ].join(".") );		//parse attributes
+		var attributes = {};
+		Object.keys(d.dimension).forEach(function(value) {
+			if(d.dimension[value]) { 				//check if dimension is selected
+				attributes[value] = [ value, d.category[value] ].join(".");		//parse attributes
 			}
-		})
+		});
+
+		var cardinals = {};
+		if(d.cardinals) {			//if there is a cardinal property
+			Object.keys(d.cardinals).forEach(function(value,index, arr) { //parsing cardinal values
+				d.cardinals[value].forEach(function(cardinal,i,arr) {
+
+					if(!cardinals[value])
+						cardinals[value] = [];
+
+					if(arr[cardinal]) {			//cardinal value was selected
+						cardinals[value].push(cardinal.trim());
+					}
+				});
+				//combine attribute and cardinals
+				attributes[value] += cardinals[value].join("+");
+			});
+		}
+
+		console.log(attributes,cardinals);
+
+		//combining cardinals and attributes
 
 		//parsed params
 		var qParams = {
 			action: d.action,
-			dimension: attributes.join(",")
+			// dimension: attributes.join(","),
+			dimension: "",
+
+			// cardinals: cardinals.join('+')
 		}
-		return qParams
+
+		return qParams;
+	}
+
+
+	/**
+	  * Get the cardinal of datas
+ 	  */
+	var parseCardinalValues = function(d) {
+		
+		var keys = d["attributes"];
+		var data = d["data"];
+		var result = {};
+
+		data.forEach(function(val,index){
+			var cardinalValues = val.key;
+			cardinalValues.forEach(function(c,i){
+				var castedCardinal = [c," "].join("");	//casting the value to a string
+				if(!result[keys[i]]) //base case when there isnt a array initialized
+					result[keys[i]] = [];
+
+				if(result[keys[i]].indexOf(castedCardinal) == -1)  //check if value is in there
+					result[keys[i]].push(castedCardinal);
+
+				// console.log(result);
+			});
+		});
+		console.log(result);
+		return result;
 	}
 
 	return {
-		parseData: parseData,
+		parseParam: parseParam,
+		parseCardinalValues: parseCardinalValues,
 		defaultFormValues: defaultFormValues
 	}
 });
