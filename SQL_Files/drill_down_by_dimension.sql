a90917a15969b5a3e0d9c2657389b558d82cb66b
Anacleto Duplito <duplito_aejr@hotmail.com>
2016-05-12 18:22:30 -0700

    Delete drill_down_by_dimension.sql

--- SQL_Files/drill_down_by_dimension.sql
+++
@@ -1,6 +0,0 @@
-SELECT p.package_size, p.category, s.city, s.store_state, t.year, sum(f.dollar_sales) AS sales_total
-FROM Product p, Store s, Date_time t, Sales f
-WHERE f.product_key =p.product_key AND
-             f.store_key = s.store_key AND
-             f.time_key = t.time_key
-GROUP BY p.package_size, p.category, s.city, s.store_state, t.year;
